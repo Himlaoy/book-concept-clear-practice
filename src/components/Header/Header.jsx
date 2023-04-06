c75b9ab515d8f1272f5c81424ada4628b24d3db0
himaloyash <himaloyashis98@gmail.com>
2023-04-06 22:49:45 +0600

    Lind is almost done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { BeakerIcon, Bars3BottomLeftIcon, BoltIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [isMenu, setIsMenu] = useState(false)
     return (
-        <nav>
-            <Link to={'/'}>Home</Link>
-            <Link to={'about'}>About</Link>
-            <Link to={'/contact'}>Contact</Link>
-            <Link to={'address'}>Address</Link>
-            
-        </nav>
+        <div className=' bg-red-300 flex justify-between p-5'>
+            <div>
+                <Link to={'/'} className='inline-flex ml-3 items-center'>
+                    <BoltIcon className='w-8 h-8' />
+                    <span className='ml-4'>Next Page</span>
+                </Link>
+            </div>
+
+            <ul className='items-center space-x-8 lg:flex'>
+                <li>
+                    <NavLink to={'/'} className={({ isActive }) => (isActive ? 'isActive' : 'default')}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/book'} className={({ isActive }) => (isActive ? 'isActive' : 'default')}>
+                        Book
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={'about'} className={({ isActive }) => (isActive ? 'isActive' : 'default')}>
+                        About
+                    </NavLink>
+                </li>
+            </ul>
+            <div>
+                <button
+                aria-label='Open menu' 
+                title='Open menu'
+                onClick={()=>setIsMenu(true)}
+                >
+
+                </button>
+            </div>
+
+        </div>
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <h2>kire beta kemon achos?</h2>
+            <Outlet></Outlet>
         </div>
     );
 };
